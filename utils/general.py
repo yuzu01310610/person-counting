14014aeb3b89c315be7af6a74d3029f12c143af0
yuzu01310610 <shunsukeyuzu00@gmail.com>
2022-09-23 05:06:47 +0900

    first commit

--- utils/general.py
+++ utils/general.py
@@ -319,7 +319,7 @@ def check_git_status(repo='ultralytics/yolov5', branch='master'):
     LOGGER.info(s)
 
 
-def check_python(minimum='3.7.0'):
+def check_python(minimum='3.6.9'):
     # Check current python version vs. required python version
     check_version(platform.python_version(), minimum, name='Python ', hard=True)
 
