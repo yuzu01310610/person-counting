cb29a47d0d35153812e73378dd0abe58d177d545
yuzu01310610 <shunsukeyuzu00@gmail.com>
2022-09-25 01:35:58 +0900

    画像取得

---
+++ webcam_img.py
@@ -0,0 +1,21 @@
+import cv2
+import datetime
+import schedule
+import time
+
+deviceid=0 # it depends on the order of USB connection. 
+capture = cv2.VideoCapture(deviceid)
+
+def job():
+    ret, frame = capture.read()
+    strdate=datetime.datetime.now().strftime('%Y%m%dT%H%M%S') 
+    fname="./save_img/image_" + strdate + ".jpg"
+    cv2.imwrite(fname, frame) 
+    print(fname + " is created.")
+
+#do job every 10 seconds
+schedule.every(1/6).minutes.do(job)
+
+while True:
+  schedule.run_pending()
+  time.sleep(1)
