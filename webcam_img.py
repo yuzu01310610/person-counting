92fae1ab147981fd0849a6f745042e90d5173555
yuzu01310610 <shunsukeyuzu00@gmail.com>
2022-09-26 01:24:01 +0900

    ここで開発

--- webcam_img.py
+++ webcam_img.py
@@ -2,14 +2,14 @@ import cv2
 import datetime
 import schedule
 import time
-
+import monitoring_img
 deviceid=0 # it depends on the order of USB connection. 
 capture = cv2.VideoCapture(deviceid)
 
 def job():
     ret, frame = capture.read()
-    strdate=datetime.datetime.now().strftime('%Y%m%dT%H%M%S') 
-    fname="./save_img/image_" + strdate + ".jpg"
+    #strdate=datetime.datetime.now().strftime('%Y%m%dT%H%M%S') 
+    fname= "./save_img/person.jpg"  #"./save_img/image_" + strdate + ".jpg"
     cv2.imwrite(fname, frame) 
     print(fname + " is created.")
 
